28e08d3bd8523132d22aed2609bc9fd0d6112649
Andrei Sarakeev <sarakusha@gmail.com>
2024-08-19 10:59:39 +0300

    build: eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  //  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    ignores: ['node_modules', 'build', 'eslint.config.mjs', '.prettierrc.cjs'],
+  }
+);
